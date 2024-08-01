82f2558471085e0f6c9fbfe6a44838f5bf6f8713
Lucas Marcelo <lucasmarcelo726@gmail.com>
2024-08-01 16:36:41 -0300

    mudança

---
+++ tipos e variaveis/src/certo.java
@@ -0,0 +1,25 @@
+
+public class certo {
+
+    public static void main (String [] args) {
+    
+        int numero1 = 150;
+        int numero2 = 75;
+    
+        int soma = soma (numero1, numero2);
+    
+    
+        System.out.println("O resultado da soma é: ");
+        System.out.println(soma);
+    
+    
+    }    
+    public static int soma (int numero1,int numero2) {
+        return + numero1 + numero2;
+    }
+    
+    
+    }
+    
+    
+    
---
+++ tipos e variaveis/src/teste.java
@@ -0,0 +1,28 @@
+public class teste {
+    
+    public static void main(String[] args) {
+        
+        int numero1 = 10;
+        int numero2 = 15;
+
+        int soma = soma (numero1,numero2);
+
+        System.out.println ("O resultado da soma é : " + soma) ;
+
+
+
+        
+    }
+
+    
+    public static int soma ( int numero1, int numero2) { 
+        return numero1+numero2;
+    
+    }
+    
+        
+        
+
+
+    
+}
